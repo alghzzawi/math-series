2ae24784d5884f68d03162e4665b1cb655658a6b
alghzzawi <mohammadalghazzawi9@gmail.com>
2022-10-14 00:02:06 +0300

    Add doc

--- module/series.py
+++ module/series.py
@@ -1,4 +1,9 @@
-def fibonacci(n):
+def fibonacci(n): 
+    """
+    fibonacci that calculate the fibonacci of numbers by using recursion
+    e.x: the fibonacci number 2 is increasing the fibonacci number 1 and fibonacci number 0
+    and note that the fibonacci 0 is 0 and fibonacci 1 is 1
+    """
     if n==0:
         return 0
     elif n==1:
@@ -7,6 +12,11 @@ def fibonacci(n):
         return fibonacci(n-1) + fibonacci(n-2)
 
 def lucas(n):
+    """
+    lucas that calculate the lucas of numbers by using recursion
+    e.x: the lucas number 2 is increasing the lucas number 1 and lucas number 0
+    and note that the fibonacci 0 is 2 and fibonacci 1 is 1
+    """
     if n==0:
         return 2
     elif n==1:
@@ -15,9 +25,15 @@ def lucas(n):
         return lucas(n-1) + lucas(n-2)
 
 def sum_series(n,first=0,sec=1):
+    """
+    this way make the input first number and sec number by default 0 and 1
+    but I late the user to change these default value
+    """
     if n==0:
         return first
     elif n==1:
         return sec
     else:
         return sum_series(n-1,first,sec) + sum_series(n-2,first,sec)
+
+
